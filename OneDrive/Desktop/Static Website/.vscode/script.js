bc56414eb556875671045cd9965283371a7e6ac3
Chandu <vinjamurichandrasekhar123@gmail.com>
2024-03-16 10:26:20 +0530

    second commit

---
+++ OneDrive/Desktop/Static Website/.vscode/script.js
@@ -0,0 +1,26 @@
+let colorPickerContainer = document.getElementById("colorPickerContainer");
+let selectedColorHexCode = document.getElementById("selectedColorHexCode");
+
+function button1() {
+    let button1Element = document.getElementById("button1");
+    colorPickerContainer.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCode.textContent = "#e0e0e0";
+}
+
+function button2() {
+    let button1Element = document.getElementById("button2");
+    colorPickerContainer.style.backgroundColor = "#6fcf97";
+    selectedColorHexCode.textContent = "#6fcf97";
+}
+
+function button3() {
+    let button1Element = document.getElementById("button3");
+    colorPickerContainer.style.backgroundColor = "#56ccf2";
+    selectedColorHexCode.textContent = "#56ccf2";
+}
+
+function button4() {
+    let button1Element = document.getElementById("button4");
+    colorPickerContainer.style.backgroundColor = "#bb6bd9";
+    selectedColorHexCode.textContent = "#bb6bd9";
+}
